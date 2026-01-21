52141e7f810c949b478fa749083177dfbdcb4600
hamza01kfg <abdulwaheed050301@gmail.com>
2026-01-21 21:06:26 +0500

    Add Firebase, EmailJS, and offer configurations

---
+++ firebase-setup.js
@@ -0,0 +1,25 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB3e5fG7K8jLmN9vXzQpR2sT1uVwXyZabC",
+    authDomain: "ht-service-project.firebaseapp.com",
+    projectId: "ht-service-project",
+    storageBucket: "ht-service-project.appspot.com",
+    messagingSenderId: "123456789012",
+    appId: "1:123456789012:web:abcdef1234567890"
+};
+
+// EmailJS Configuration
+const emailjsConfig = {
+    userID: "q_cI26sBuHJYeJ7OG",
+    serviceID: "service_n2r2our",
+    templateID: "template_rqvrxoh"
+};
+
+// Offer Configuration
+const offerConfig = {
+    active: true,
+    startDate: "2024-01-01",
+    endDate: "2024-12-31",
+    percentage: 30,
+    description: "Special year-end discount on all products"
+};
